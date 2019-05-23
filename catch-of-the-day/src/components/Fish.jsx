a8d51968e0bde5f6722cbbdd75c41d8f522b1417
Neil Enns <neile@adobe.com>
2019-05-23 10:53:49 -0700

    Minor refactor of the event handler

--- catch-of-the-day/src/components/Fish.jsx
+++ catch-of-the-day/src/components/Fish.jsx
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import { formatPrice } from "../helpers";
 
 class Fish extends Component {
-  handleClick = () => {
-    this.props.addToOrder(this.props.fishId);
-  };
-
   render() {
     const { image, name, price, desc, status } = this.props.details;
     const isAvailable = status === "available";
@@ -18,7 +14,10 @@ class Fish extends Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button disabled={!isAvailable} onClick={this.handleClick}>
+        <button
+          disabled={!isAvailable}
+          onClick={() => this.props.addToOrder(this.props.fishId)}
+        >
           {isAvailable ? "Add To Cart" : "Sold Out!"}
         </button>
       </ul>
