e63035c20cd357421bffa6f6fe6edbc8bdded8a1
Neil Enns <neile@adobe.com>
2019-05-23 10:39:35 -0700

    Fish entries render

--- catch-of-the-day/src/components/App.jsx
+++ catch-of-the-day/src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+import Fish from "./Fish";
 import sampleFishes from "../sample-fishes";
 
 class App extends Component {
@@ -29,6 +30,9 @@ class App extends Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          {Object.keys(this.state.fishes).map(key => (
+            <Fish key={key} details={this.state.fishes[key]} />
+          ))}
         </div>
         <Order />
         <Inventory
