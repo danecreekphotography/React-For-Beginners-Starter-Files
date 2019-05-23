04e924d5e8561312305c9a078443248291be1e03
Neil Enns <neile@adobe.com>
2019-05-23 13:50:23 -0700

    Local storage of an order

--- catch-of-the-day/src/components/App.jsx
+++ catch-of-the-day/src/components/App.jsx
@@ -14,11 +14,25 @@ class App extends Component {
 
   componentDidMount() {
     const { params } = this.props.match;
+    const localStorageRef = localStorage.getItem(`order-${params.storeId}`);
+
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
+    }
 
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: "fishes"
     });
+  }
+
+  componentDidUpdate() {
+    const { params } = this.props.match;
+
+    localStorage.setItem(
+      `order-${params.storeId}`,
+      JSON.stringify(this.state.order)
+    );
   }
 
   componentWillUnmount() {
